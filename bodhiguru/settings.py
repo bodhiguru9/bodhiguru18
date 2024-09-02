16a4ba9f681447dd19550662f5ee3805148db14d
bodhiguru9 <bodhiguru7@gmail.com>
2024-09-02 10:55:53 +0530

    commit 3.35

--- bodhiguru/settings.py
+++ bodhiguru/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = get_env_variable('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = get_env_variable('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bodhiguru-env.eba-fu2rg3s5.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
