fb385eaf9c8957a7d5f77f6bb1a77840c9ed7dc1
bodhiguru9 <bodhiguru7@gmail.com>
2024-09-03 10:02:29 +0530

    commit 3.35

--- accounts/models.py
+++ accounts/models.py
@@ -10,7 +10,7 @@ from orgss.models import Org, Role
 from uuid import uuid4
 
 class MyAccountManager(BaseUserManager):
-    def create_user(self, first_name, last_name, email, username, contact_number, password=None):
+    def create_user(self, first_name, last_name, email, username, password=None):
         if not email:
             raise ValueError("User must have an email address")
 
@@ -22,7 +22,6 @@ class MyAccountManager(BaseUserManager):
             username = username,
             first_name = first_name,
             last_name = last_name,
-            contact_number = contact_number,
 
         )        
 
@@ -30,14 +29,13 @@ class MyAccountManager(BaseUserManager):
         user.save(using = self.db)
         return user
 
-    def create_superuser(self, first_name, last_name, email, contact_number, username, password):
+    def create_superuser(self, first_name, last_name, email, username, password):
         user = self.create_user(
             email = self.normalize_email(email),
             first_name = first_name,
             last_name = last_name,
             username = username,
             password = password,
-            contact_number = contact_number,
 
         )   
 
