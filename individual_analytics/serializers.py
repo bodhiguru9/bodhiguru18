67524752d76f9597d8d3576cb321fb8082b6c554
bodhiguru9 <bodhiguru7@gmail.com>
2024-09-05 14:59:13 +0530

    commit 3.39

--- individual_analytics/serializers.py
+++ individual_analytics/serializers.py
@@ -1,24 +1,16 @@
 from rest_framework import serializers
 from accounts.models import UserProfile, Account
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = ['scenarios_attempted', 'scenarios_attempted_score', 'user_powerwords', 'user_weakwords', 'competency_score', 'current_level']
+        fields = ['scenarios_attempted', 'scenarios_attempted_score',
+                    'user_powerwords', 'user_weakwords', 'competency_score', 'current_level']
+        
 
-class UserSerializer(serializers.ModelSerializer):
-    profile = UserProfileSerializer(source='userprofile')
-
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'profile']
-
-class UserSerializer1(serializers.ModelSerializer):
-    profile = UserProfileSerializer(source='userprofile')
+class AccountSerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer()
 
     class Meta:
         model = Account
-        fields = '__all__'      
+        fields = ['email', 'org', 'role','profile']
--- individual_analytics/views.py
+++ individual_analytics/views.py
@@ -1,54 +1,41 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.exceptions import PermissionDenied
+from .serializers import AccountSerializer, UserProfileSerializer
 from accounts.models import Account, UserProfile
-from individual_analytics.serializers import UserSerializer1, UserSerializer
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-class IsAdminOrSubAdmin:
-    def has_permission(self, request, view):
-        try:
-            account = Account.objects.get(user=request.user)
-            role = account.role.role_type
-            return role in ['admin', 'sub_admin']
-        except Account.DoesNotExist:
-            return False
+from .permissions import IsAdminOrSubAdmin
+from django.shortcuts import get_object_or_404
+from rest_framework.exceptions import PermissionDenied
 
 class UserListView(generics.ListAPIView):
-    serializer_class = UserSerializer1
     permission_classes = [IsAuthenticated, IsAdminOrSubAdmin]
+    serializer_class = AccountSerializer
 
     def get_queryset(self):
-        user = self.request.user
-        try:
-            account = Account.objects.get(user=user)
-            role = account.role.role_type
-            if role in ['admin', 'sub_admin']:
-                # Get all accounts in the same org and fetch related users
-                accounts_in_org = Account.objects.filter(org=account.org)
-                return Account.objects.filter(account__in=accounts_in_org)
-            else:
-                raise PermissionDenied("You do not have permission to view this data.")
-        except Account.DoesNotExist:
-            return Account.objects.none()
+        # Get the admin/sub_admin's org using their email
+        account = get_object_or_404(Account, email=self.request.user.email)
+        sub_org = self.request.query_params.get('sub_org')
+        
+        if sub_org:
+            # Return users within the same org and filtered by sub_org
+            return Account.objects.filter(org=account.org, sub_org_id=sub_org)
+        
+        # Return users within the same org
+        return Account.objects.filter(org=account.org)
 
 class UserDetailView(generics.RetrieveAPIView):
-    serializer_class = UserSerializer
     permission_classes = [IsAuthenticated, IsAdminOrSubAdmin]
-    lookup_field = 'pk'
+    serializer_class = UserProfileSerializer
 
-    def get_queryset(self):
-        user = self.request.user
-        try:
-            account = Account.objects.get(user=user)
-            role = account.role.role_type
-            if role in ['admin', 'sub_admin']:
-                # Get all accounts in the same org and fetch related users
-                accounts_in_org = Account.objects.filter(org=account.org)
-                return User.objects.filter(account__in=accounts_in_org)
-            else:
-                raise PermissionDenied("You do not have permission to view this data.")
-        except Account.DoesNotExist:
-            return User.objects.none()
+    def get_object(self):
+        # Retrieve the user profile by email and ensure they are part of the same org/sub-org
+        email = self.kwargs['email']
+        user_profile = get_object_or_404(UserProfile, user__email=email)
+        
+        # Get the admin/sub_admin's org using their email
+        account = get_object_or_404(Account, email=self.request.user.email)
+
+        # Check if the user belongs to the same org
+        if user_profile.user.account.org == account.org:
+            return user_profile
+        else:
+            raise PermissionDenied("You do not have permission to view this user's data.")
