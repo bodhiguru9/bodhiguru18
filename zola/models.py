cd1dc30646e51664ee5c2e3f156b6978dacf33dc
bodhiguru9 <bodhiguru7@gmail.com>
2024-09-12 11:13:15 +0530

    commit 3.316

--- zola/models.py
+++ zola/models.py
@@ -57,8 +57,7 @@ class ItemResult(models.Model):
     score = models.IntegerField(default=0)
     
     def __str__(self):
-        def __str__(self):
-            return f'{self.user.email} - {self.item.name} - {self.score}'
+        return f'{self.user.email} - {self.item.name} - {self.score}'
   
 
 class Suggestion(models.Model):
