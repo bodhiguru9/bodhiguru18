4a5f9aa185dd0ce53613d6c65805d49610a4b754
bodhiguru9 <bodhiguru7@gmail.com>
2024-09-20 11:43:49 +0530

    commit 3.323

--- zola/models.py
+++ zola/models.py
@@ -41,6 +41,7 @@ class Item(models.Model):
     coming_across_as = models.CharField(max_length=250, null=True, blank=True)
     competencys = models.ManyToManyField(Competency, blank=True)
     words = models.TextField(default = "words")
+    library_filter = models.CharField(max_length=700, default="filter")
     role = models.ForeignKey(Role1, on_delete=models.CASCADE, related_name='seanrole', null=True, blank=True)
     level = models.IntegerField(default=1)
     expert = models.URLField(blank = True, null=True)
