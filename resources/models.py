06a4f3b7128933ce2b3531e64253c581845db5d9
bodhiguru9 <bodhiguru7@gmail.com>
2024-09-21 05:44:55 +0530

    commit 3.324

--- resources/models.py
+++ resources/models.py
@@ -12,6 +12,8 @@ class Resource(models.Model):
 class TextResouce(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+    video = models.URLField(max_length=200, blank=True, null=True, default = 'https://www.youtube.com/watch?v=tfrvVKM0DZY')
+
     
 
     def __str__(self):
