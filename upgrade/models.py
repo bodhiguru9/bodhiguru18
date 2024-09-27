a6e55c0386d26092dc653d4f8730ac1d66313a1a
bodhiguru9 <bodhiguru7@gmail.com>
2024-09-27 15:32:05 +0530

    commit 3.326

--- upgrade/models.py
+++ upgrade/models.py
@@ -23,8 +23,8 @@ class Upgradedetail(models.Model):
     upgrade = models.ForeignKey(Upgrade, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     transaction_details = models.TextField()
-    additional_logins = models.IntegerField(default=0)  # For storing additional logins for Package 9
-    expires_on = models.DateField(default=timezone.now().date() + timedelta(days=30))
+    #additional_logins = models.IntegerField(default=0)  # For storing additional logins for Package 9
+    #expires_on = models.DateField(default=timezone.now().date() + timedelta(days=30))
 
     def __str__(self):
         return f"{self.org} - {self.upgrade}"
