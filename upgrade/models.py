8efe135e476121f33817e65b060ef63d82ca83e6
bodhiguru9 <bodhiguru7@gmail.com>
2024-09-30 17:10:23 +0530

    commit 3.328

--- upgrade/models.py
+++ upgrade/models.py
@@ -24,7 +24,7 @@ class Upgradedetail(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     transaction_details = models.TextField()
     #additional_logins = models.IntegerField(default=0)  # For storing additional logins for Package 9
-    #expires_on = models.DateField(default=timezone.now().date() + timedelta(days=30))
+    expires_on = models.DateField(default=timezone.now().date() + timedelta(days=30))
 
     def __str__(self):
         return f"{self.org} - {self.upgrade}"
